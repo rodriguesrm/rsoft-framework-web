a3a5919317130e55d7deccb9a44a51407c593984
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2020-10-12 07:26:53 -0300

    Added health check endpoint method

---
+++ src/RSoft.Framework.Web/Extensions/HealthCheckExtension.cs
@@ -0,0 +1,34 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace RSoft.Framework.Web.Extensions
+{
+
+    /// <summary>
+    /// Provides extension methods for Health Check
+    /// </summary>
+    public static class HealthCheckExtension
+    {
+
+        /// <summary>
+        /// Adds a middleware that provides health check status.
+        /// </summary>
+        /// <param name="app">Application builder object instance</param>
+        public static IApplicationBuilder UseApplicationHealthChecks(this IApplicationBuilder app)
+        {
+
+            app
+                .UseHealthChecks("/hc", new HealthCheckOptions
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
+
+            return app;
+        }
+
+    }
+
+}
